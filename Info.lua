33a33832720f910f6ff6b3f3b26723f9cb08cda3
Thomas Hackl <thomas@thomas-hackl.name>
2019-12-23 16:20:41 +0100

    Add plugin metadata file

---
+++ Info.lua
@@ -0,0 +1,33 @@
+--[[----------------------------------------------------------------------------
+
+Info.lua
+Summary information for X3 photo gallery plug-in
+
+--------------------------------------------------------------------------------
+
+Thomas Hackl
+ Copyright 2019 Adobe Systems Incorporated
+ All Rights Reserved.
+
+This plug-in provides photo upload via FTP to a remote server and generating
+the correct page.json with metadata needed for X3 photo gallery.
+
+------------------------------------------------------------------------------]]
+
+return {
+
+	LrSdkVersion = 3.0,
+	LrSdkMinimumVersion = 1.3, -- minimum SDK version required by this plug-in
+
+	LrToolkitIdentifier = 'name.thomas-hackl.lightroom.export.x3',
+
+	LrPluginName = LOC "$$$/FTPUpload/PluginName=X3 Photo Gallery",
+
+	LrExportServiceProvider = {
+		title = "X3 Photo Gallery",
+		file = 'X3GalleryServiceProvider.lua',
+	},
+
+	VERSION = { major=1, minor=0, revision=0, build="201912231547", },
+
+}
